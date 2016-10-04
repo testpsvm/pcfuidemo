91ee69d1fbb56c65cd3811e011b802239a261c70
vincent <vincent@vincent.fr>
2016-10-04 22:29:43 +0200

    CRUD User

--- user/user.js
+++ user/user.js
@@ -4,7 +4,7 @@ var userModule = angular.module('userModule', ['ngResource']);
 
 
 var userFactory = userModule.factory('userFactory', ['$resource', function($resource) {
-  return $resource('http://localhost:9999/user/:userId', {userId : '@userId'});
+  return $resource('http://mtpcfdemo.cfapps.io/user/:userId', {userId : '@userId'});
 }]);
 
 
@@ -14,31 +14,41 @@ var userFactory = userModule.factory('userFactory', ['$resource', function($reso
 userModule.controller('userController', ['$scope', 'userFactory', function ($scope, userFactory) {
   $scope.userList =   userFactory.query();
 
-  $scope.restActionMessage = '';
+  $scope.sortColumn =   'name';
+  
+  $scope.restCreateActionMessage = '';
+  $scope.restDeleteActionMessage = '';
 
   $scope.newuser = {
+	userId:"",
     name:"",
     firstName:"",
     showDel:false
   };
 
-
+  
 
   // methods
   $scope.addUser = function (user) {
-      // we push the new values
-      $scope.userList.push({name:user.name, firstName:user.firstName, showDel:false});
-      // we reinit the new user attributes
-      $scope.newuser.name= '';
-      $scope.newuser.firstName= '';
-      $scope.newuser.showDel=false;
+	  userFactory.save(user).$promise.then(function(data) {
+	  	  $scope.userList = userFactory.query();
+	      // we reinit the new user attributes
+		  $scope.newuser.name= '';
+		  $scope.newuser.firstName= '';
+		  $scope.newuser.showDel=false;
+		  $scope.restCreateActionMessage = 'Creation successful';
+	      $scope.restDeleteActionMessage = '';
+	  });
   };
 
+  // remove user action
   $scope.removeUser = function (user) {
-        //var index = $scope.userList.indexOf(user);
-        //$scope.userList.splice(index, 1);
-        userFactory.delete(user.userId);
-        restActionMessage = 'ok';
+	  // call the delete remote service
+	  userFactory.delete(user).$promise.then(function(data) {
+	      $scope.userList = userFactory.query();
+	      $scope.restCreateActionMessage = '';
+	      $scope.restDeleteActionMessage = data.message;
+      });
   };
 
 
